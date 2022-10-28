b87831ab89b980325802b052e000e9a1f9f7effa
Alex Roth <aroth6@asu.edu>
2022-10-27 23:26:13 -0700

    lines

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
 vim.cmd [[packadd packer.nvim]]
-
 require("config/cmp")
 require("config/lang")
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.expandtab = false
+vim.o.tabstop=4
+vim.o.shiftwidth=4
 return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'hrsh7th/cmp-nvim-lsp'
@@ -13,11 +15,11 @@ return require('packer').startup(function(use)
   use 'hrsh7th/nvim-cmp'
   use 'onsails/lspkind.nvim'
   use 'ryanoasis/vim-devicons'
-	use 'wbthomason/packer.nvim'
+  use 'wbthomason/packer.nvim'
   use 'hrsh7th/vim-vsnip'
   use 'hrsh7th/vim-vsnip-integ'
-  use'https://github.com/vim-airline/vim-airline'
-  use "https://git.sr.ht/~whynothugo/lsp_lines.nvim"
+  use 'https://github.com/vim-airline/vim-airline'
+  use 'https://git.sr.ht/~whynothugo/lsp_lines.nvim'
   use 'sheerun/vim-polyglot'
   use 'simrat39/rust-tools.nvim'
   use 'scrooloose/NERDTree'
